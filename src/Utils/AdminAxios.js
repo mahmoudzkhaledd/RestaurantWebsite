a374df1f55ca3fbc2509bef0b071a3df5168eca7
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-02-02 09:36:56 +0200

    Update AdminAxios.js

--- src/Utils/AdminAxios.js
+++ src/Utils/AdminAxios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 const adminAxios = axios.create({
     baseURL: process.env.NODE_ENV != 'development' ?
-        'https://medical-back.vercel.app/admin' :
+        'https://restaurant-rose-five.vercel.app/admin' :
         "http://192.168.1.8:3000/admin",
     withCredentials: true,
     headers: {
